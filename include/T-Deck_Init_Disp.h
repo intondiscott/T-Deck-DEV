924c08c8b9a58f8a777fc0e4dbc7bea58c183564
Scotty <73749875+intondiscott@users.noreply.github.com>
2024-01-01 09:24:37 -0500

    Add files via upload

---
+++ include/T-Deck_Init_Disp.h
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <utilities.h>
+#include <SPI.h>
+class Init_Display
+{
+public:
+  Init_Display(void)
+  {
+    //! The board peripheral power control pin needs to be set to HIGH when using the peripheral
+    pinMode(BOARD_POWERON, OUTPUT);
+    digitalWrite(BOARD_POWERON, HIGH);
+
+    //! Set CS on all SPI buses to high level during initialization
+    pinMode(BOARD_SDCARD_CS, OUTPUT);
+    pinMode(RADIO_CS_PIN, OUTPUT);
+    pinMode(BOARD_TFT_CS, OUTPUT);
+
+    digitalWrite(BOARD_SDCARD_CS, HIGH);
+    digitalWrite(RADIO_CS_PIN, HIGH);
+    digitalWrite(BOARD_TFT_CS, HIGH);
+
+    pinMode(BOARD_SPI_MISO, INPUT_PULLUP);
+    SPI.begin(BOARD_SPI_SCK, BOARD_SPI_MISO, BOARD_SPI_MOSI); // SD
+
+    pinMode(BOARD_BOOT_PIN, INPUT_PULLUP);
+    pinMode(BOARD_TBOX_G02, INPUT_PULLUP);
+    pinMode(BOARD_TBOX_G01, INPUT_PULLUP);
+    pinMode(BOARD_TBOX_G04, INPUT_PULLUP);
+    pinMode(BOARD_TBOX_G03, INPUT_PULLUP);
+    
+
+    // Wakeup touch chip
+    pinMode(BOARD_TOUCH_INT, OUTPUT);
+    digitalWrite(BOARD_TOUCH_INT, HIGH);
+  }
+};
